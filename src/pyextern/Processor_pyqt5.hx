3417180e676bc5fc5a20d8cb3037825b9344b6fe
Andy Li <andy@onthewings.net>
2018-11-05 17:18:57 +0800

    handle complex pyqt5 method sig

--- src/pyextern/Processor_pyqt5.hx
+++ src/pyextern/Processor_pyqt5.hx
@@ -9,13 +9,32 @@ using StringTools;
 
 @process_modules("PyQt5")
 class Process_pyqt5 extends Processor {
+	static function removeBrackets(str:String, open:String, close:String):String {
+		var result = "";
+		var opened = 0;
+		for (i in 0...str.length) {
+			var c = str.charAt(i);
+			if (c == open)
+				opened++;
+			else if (c == close)
+				opened--;
+			else if (opened == 0)
+				result += c;
+		}
+		return result;
+	}
 	override public function sigToFun(sig:Dynamic, doc:Null<String>):Function {
 		if (sig == null && doc != null) {
-			var funcR = ~/^(.+)\((.*)\)(?:\s*->\s*(.+))?$/;
+			var funcR = ~/^(.+?)\((.*)\)(?:\s*->\s*(.+))?$/;
 			if (funcR.match(doc)) {
 				var funcName = funcR.matched(1);
 				var funcArgs = funcR.matched(2);
-				funcArgs = ~/Union\[.+?\]/g.replace(funcArgs, "arg");
+				if (funcArgs.indexOf("(") >= 0) {
+					funcArgs = removeBrackets(funcArgs, "(", ")");
+				}
+				if (funcArgs.indexOf("[") >= 0) {
+					funcArgs = removeBrackets(funcArgs, "[", "]");
+				}
 				var ret = funcR.matched(3);
 				var args = funcArgs == "" ? [] : funcArgs.split(",").map(StringTools.trim).map(function(a) {
 					var name = ~/^[A-Za-z0-9]+$/.match(a) ? a : {
