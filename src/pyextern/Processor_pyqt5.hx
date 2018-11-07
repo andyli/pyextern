b50487bc7d188dd04772cd239df48ece9633960b
Andy Li <andy@onthewings.net>
2018-11-07 15:24:51 +0800

    improve handling of pyqt5 overloaded functions

--- src/pyextern/Processor_pyqt5.hx
+++ src/pyextern/Processor_pyqt5.hx
@@ -41,42 +41,77 @@ class Process_pyqt5 extends Processor {
 	}
 	override public function sigToFun(sig:Dynamic, doc:Null<String>):Function {
 		if (sig == null && doc != null) {
+			var docLines = doc.split("\n");
 			var funcR = ~/^(.+?)\((.*)\)(?:\s*->\s*(.+))?$/;
-			if (funcR.match(doc)) {
-				var funcName = funcR.matched(1);
-				var funcArgs = funcR.matched(2);
-				if (funcArgs.indexOf("(") >= 0) {
-					funcArgs = removeBrackets(funcArgs, "(", ")");
-				}
-				if (funcArgs.indexOf("[") >= 0) {
-					funcArgs = removeBrackets(funcArgs, "[", "]");
-				}
-				var ret = funcR.matched(3);
-				var args = funcArgs == "" ? [] : funcArgs.split(",").map(StringTools.trim).map(function(a) {
-					var name = ~/^[A-Za-z0-9]+$/.match(a) ? a : {
-						var r = ~/^[A-Za-z0-9]+/;
-						if (r.match(a)) {
-							r.matched(0);
-						} else {
-							"_";
+			if (docLines.length > 0 && docLines.foreach(funcR.match)){
+				var funcs = [
+					for (doc in docLines)
+					if (funcR.match(doc)) {
+						var funcName = funcR.matched(1);
+						var funcArgs = funcR.matched(2);
+						if (funcArgs.indexOf("(") >= 0) {
+							funcArgs = removeBrackets(funcArgs, "(", ")");
+						}
+						if (funcArgs.indexOf("[") >= 0) {
+							funcArgs = removeBrackets(funcArgs, "[", "]");
+						}
+						var ret = funcR.matched(3);
+						var args = funcArgs == "" ? [] : funcArgs.split(",").map(StringTools.trim).map(function(a) {
+							var name = ~/^[A-Za-z0-9]+$/.match(a) ? a : {
+								var r = ~/^[A-Za-z0-9]+/;
+								if (r.match(a)) {
+									r.matched(0);
+								} else {
+									"_";
+								}
+							}
+							return ({
+								opt: a.indexOf("=") >= 0,
+								name: name,
+								type: switch (hxType(name)) {
+									case null: macro:Dynamic;
+									case ct: ct;
+								},
+							}:FunctionArg);
+						});
+						{
+							params:[],
+							args: args,
+							ret: ret == null ? macro:Void : switch (hxType(ret)) {
+								case null: macro:Dynamic;
+								case ct: ct;
+							},
+							expr: null
 						}
 					}
-					return ({
-						opt: a.indexOf("=") >= 0,
-						name: name,
-						type: switch (hxType(name)) {
-							case null: macro:Dynamic;
-							case ct: ct;
-						},
-					}:FunctionArg);
-				});
+				];
+				var sameArgs = funcs.fold(function(f:Function, sameArgs:Array<FunctionArg>){
+					var allSame = true;
+					return [
+						for (i in 0...sameArgs.length)
+						if (
+							allSame && (
+								allSame = (
+									i < f.args.length &&
+									f.args[i].name == sameArgs[i].name &&
+									f.args[i].opt == sameArgs[i].opt
+								)
+							)
+						)
+						sameArgs[i]
+					];
+				}, funcs[0].args);
+
+				var hasRest = funcs.exists(function(f) return f.args.length > sameArgs.length);
+				var sameRet = funcs.foreach(function(f) return ComplexTypeTools.toString(f.ret) == ComplexTypeTools.toString(funcs[0].ret));
 				return {
 					params:[],
-					args: args,
-					ret: ret == null ? macro:Void : switch (hxType(ret)) {
-						case null: macro:Dynamic;
-						case ct: ct;
-					},
+					args: !hasRest ? sameArgs : sameArgs.concat([{
+						opt: false,
+						name: "args",
+						type: macro:haxe.extern.Rest<Dynamic>
+					}]),
+					ret: sameRet ? funcs[0].ret : macro:Dynamic,
 					expr: null
 				}
 			}
