07ff1edb3b6b8d41b8e4b6d44e3e30ba0afe8a5f
Andy Li <andy@onthewings.net>
2022-02-23 19:04:52 +0800

    test KwCall.call() with static functions

--- test/TestPyHelpers.hx
+++ test/TestPyHelpers.hx
@@ -15,6 +15,10 @@ class MyClass {
         if (b != null)
             this.b = b;
     }
+
+    static public function noop(a:Int = 0, b:Int = 0):{a:Int, b:Int} {
+        return { a: a, b: b };
+    }
 }
 
 class TestPyHelpers extends utest.Test {
@@ -51,5 +55,35 @@ class TestPyHelpers extends utest.Test {
         o.set.call(a => 1, b => 2);
         Assert.equals(1, o.a);
         Assert.equals(2, o.b);
+    }
+
+    public function testStaticMethodSimple() {
+        final v = MyClass.noop.call(1, 2);
+        Assert.equals(1, v.a);
+        Assert.equals(2, v.b);
+    }
+
+    public function testStaticMethodPartial1() {
+        final v = MyClass.noop.call(a => 1);
+        Assert.equals(1, v.a);
+        Assert.equals(0, v.b);
+    }
+
+    public function testStaticMethodPartial2() {
+        final v = MyClass.noop.call(b => 2);
+        Assert.equals(0, v.a);
+        Assert.equals(2, v.b);
+    }
+
+    public function testStaticMethodSupplyAll() {
+        final v = MyClass.noop.call(a => 3, b => 2);
+        Assert.equals(3, v.a);
+        Assert.equals(2, v.b);
+    }
+
+    public function testStaticMethodSupplyReversed() {
+        final v = MyClass.noop.call(b => 2, a => 3);
+        Assert.equals(3, v.a);
+        Assert.equals(2, v.b);
     }
 }
